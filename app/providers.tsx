a14947593df6e3180be7c9352526f16429059b72
nstanogias <nstanogias@gmail.com>
2024-10-01 23:50:11 +0300

    added sign in message functionality

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,18 +2,35 @@
 
 import { config } from "@/lib/wagmi";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  RainbowKitSiweNextAuthProvider,
+  GetSiweMessageOptions,
+} from "@rainbow-me/rainbowkit-siwe-next-auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 import React from "react";
 import { WagmiProvider } from "wagmi";
 
 const queryClient = new QueryClient();
 
+const getSiweMessageOptions: GetSiweMessageOptions = () => ({
+  statement: "Sign this message to properly sign in",
+});
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>{children}</RainbowKitProvider>
-      </QueryClientProvider>
+      <SessionProvider refetchInterval={0} session={null}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitSiweNextAuthProvider
+            getSiweMessageOptions={getSiweMessageOptions}
+          >
+            <RainbowKitProvider modalSize="compact">
+              {children}
+            </RainbowKitProvider>
+          </RainbowKitSiweNextAuthProvider>
+        </QueryClientProvider>
+      </SessionProvider>
     </WagmiProvider>
   );
 }
