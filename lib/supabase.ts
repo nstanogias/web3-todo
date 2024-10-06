9125bc0953c8944e9a6474aa73b37c0461f2a2d1
nstanogias <nstanogias@gmail.com>
2024-10-06 20:51:12 +0300

    added supabase client

---
+++ lib/supabase.ts
@@ -0,0 +1,7 @@
+// lib/supabase.ts
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
